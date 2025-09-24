a1c44bea5f2041d947c9e01271b2161b43454390
Emre Tümer <emreetumer0@gmail.com>
2025-09-24 11:33:04 +0300

    Dependency ve dependency inject etmek ne demektir

--- DependencyInjection/DependencyInjection.ConsoleApp/Program.cs
+++ DependencyInjection/DependencyInjection.ConsoleApp/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿var cekic = new Cekic();
+var civi = new Civi();
+Builder builder = new Builder(cekic, civi);
+builder.BuildHouse();
+
+class Civi //Dependency
+{
+    public void Use()
+    {
+        Console.WriteLine("Civi kullanıldı.");
+    }
+}
+
+class Cekic // Dependency
+{
+    public void Use()
+    {
+        Console.WriteLine("Cekic kullanıldı.");
+    }
+}
+
+class Builder
+{
+    Cekic _cekic;
+    Civi _civi;
+    public Builder(Cekic cekic, Civi civi)
+    {
+        _civi = civi;
+        _cekic = cekic;
+    }
+
+    public void BuildHouse()
+    {
+        //Cekic cekic = new Cekic();
+        //cekic.Use();
+        //Civi civi = new Civi();
+        //civi.Use();
+
+        _cekic.Use();
+        _civi.Use();
+
+        Console.WriteLine("Ev inşa edildi");
+    }
+}
