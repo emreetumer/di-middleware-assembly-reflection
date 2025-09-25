c436a938717fcc00556c3175b717624411a1fba9
Emre Tümer <emreetumer0@gmail.com>
2025-09-25 08:26:51 +0300

    Controller üzerinden DI ve Ctor

---
+++ DependencyInjection/DependencyInjection.WebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjection.WebApplication.Controllers;
+
+[ApiController]
+[Route("/api/[Controller]")]
+public class ValuesController : ControllerBase
+{
+    private readonly Builder _builder;
+    public ValuesController(Builder builder)
+    {
+        _builder = builder;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        _builder.BuildHouse();
+        return Ok();
+    }
+}
