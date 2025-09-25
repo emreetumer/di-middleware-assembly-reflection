90ddb0d0c768f18a2ddb3f57716ce7d47982ce92
Emre Tümer <emreetumer0@gmail.com>
2025-09-25 10:04:39 +0300

    Custom Middleware

---
+++ Middleware/Middleware.WebAPI/ExampleMiddleware.cs
@@ -0,0 +1,11 @@
+﻿namespace Middleware.WebAPI;
+
+public class ExampleMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        // context üzerinden istediğimiz işlemler yapılır.
+
+        await next(context);
+    }
+}
