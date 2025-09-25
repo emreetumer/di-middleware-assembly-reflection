976520db5e4f293219603151d6c54b40cbb6b383
Emre Tümer <emreetumer0@gmail.com>
2025-09-25 10:29:52 +0300

    Assembly giriş

---
+++ AssemblyFundamentals/AssemblyFundamentals.WebAPI/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace AssemblyFundamentals.WebAPI;
+
+public class Test
+{
+    public string Name { get; set; } = "Emre Tümer";
+    public int Age { get; set; } = 24;
+    public void Method()
+    {
+        Console.WriteLine("I am working..");
+    }
+}
+
+public class AssemblyTest
+{
+    public void Method()
+    {
+        //Buraya breakporit koyup derleyince assembly içindekileri görüyoruz.
+        var assembly = Assembly.GetExecutingAssembly();
+    }
+}
