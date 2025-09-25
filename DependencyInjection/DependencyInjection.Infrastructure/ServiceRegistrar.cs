ca29344a2dced99ee8a0317607e6d8ebeeef8ea3
Emre Tümer <emreetumer0@gmail.com>
2025-09-25 09:24:41 +0300

    Scrutor kütüphanesi

--- DependencyInjection/DependencyInjection.Infrastructure/ServiceRegistrar.cs
+++ DependencyInjection/DependencyInjection.Infrastructure/ServiceRegistrar.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
 
 namespace DependencyInjection.Infrastructure;
 public static class ServiceRegistrar
@@ -6,6 +7,15 @@ public static class ServiceRegistrar
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         //services.AddScoped<IProductService, ProductService>(); 
+
+        services.Scan(action => action
+        .FromAssemblies(typeof(ServiceRegistrar).Assembly)
+        .AddClasses(publicOnly: false)
+        .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+        .AsImplementedInterfaces()
+        .WithScopedLifetime()
+        );
+        //Bu üstteki kod scrutor kütüphanesi tarafından interfaceli dependency injection yapmanın kolay hali yani 100lerce  services.AddScoped<IProductService, ProductService>(); buna benzer kod yerine bu kodu yazarak otomatik algılamasını sağlıyor.  
         return services;
     }
 }
