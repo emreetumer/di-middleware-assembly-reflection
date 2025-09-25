2f0d10ed6dce8d89523e62a27fdc007efcc06a89
Emre Tümer <emreetumer0@gmail.com>
2025-09-25 09:18:52 +0300

    DI için Extensions metot

---
+++ DependencyInjection/DependencyInjection.Infrastructure/ServiceRegistrar.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyInjection.Infrastructure;
+public static class ServiceRegistrar
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        //services.AddScoped<IProductService, ProductService>(); 
+        return services;
+    }
+}
+
+//Bu yapı program cs ye yazmak istemessek böyle oluşturulabilir. Program cs de services.addscoped<AddInfrastructre> çağırmamız lazım (Extension method)
